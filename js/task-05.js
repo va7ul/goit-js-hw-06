4934fc76b9adc125a6ce00d86d7195da30ca763e
va7ul <vas7ul@gmail.com>
2023-06-07 16:53:17 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,5 @@ const inputEl = document.querySelector('#name-input')
 const outputEl = document.querySelector('#name-output')
 
 inputEl.addEventListener("input", (event) => {
-    outputEl.textContent = event.currentTarget.value;
+    outputEl.textContent = event.currentTarget.value === "" ? "Anonymous": event.currentTarget.value;
 });
